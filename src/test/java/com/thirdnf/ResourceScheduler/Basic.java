6a9227fcb034453957a2d5e2185b3cf8fc670d32
hrakaroo <hrakaroo@fd0fc832-696f-d648-a748-1785b494cb41>
2011-01-27 07:00:37 +0000

    Reworking to make a sub module of a parent pom

---
+++ src/test/java/com/thirdnf/ResourceScheduler/Basic.java
@@ -0,0 +1,13 @@
+package com.thirdnf.ResourceScheduler;
+
+import org.junit.Test;
+
+
+public class Basic
+{
+    @Test
+    public void doa()
+    {
+        System.out.println("ResourceScheduler Alive");
+    }
+}
